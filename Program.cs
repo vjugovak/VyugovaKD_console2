ebe6f2cb72d85c2c487ad17a0d8728f96699f928
vjugovak <vjugova.k@yandex.ru>
2025-05-31 15:31:57 +0300

    Add *.cs-files

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace VyugovaKD
+{
+    internal class Program
+    {
+        static public double Sqr (double x)
+        {
+            return x*x;
+            //Вьюгова Ксения Дмитриевна Ам-1-24
+            
+        }
+    }
+}
